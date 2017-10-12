8e0223bd6370b1f5b097baa878df15a2f7759ba8
Seena Rowhani <seenarowhani95@gmail.com>
2017-10-12 16:40:50 -0400

    py nbd file

--- a2.py
+++ a2.py
@@ -1,18 +1,31 @@
-#!/usr/bin/env python
 import json
+from IPython.display import display, Image
+
+DATA_TYPES = {
+    0x08: 'B',  # unsigned byte
+    0x09: 'b',  # signed byte
+    0x0b: 'h',  # short (2 bytes)
+    0x0c: 'i',  # int (4 bytes)
+    0x0d: 'f',  # float (4 bytes)
+    0x0e: 'd'
+}  # double (8 bytes)
 
 CACHE_DIR = '.cache'
 url  = 'http://yann.lecun.com/exdb/mnist/'
-
-from scipy import ndimage
-from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+from scipy import misc
 from six.moves.urllib.request import urlretrieve
 from six.moves import cPickle as pickle
 
+import struct
+import array
 import numpy as np
 import os
+import operator
 import sys
-import tarfile
+import functools
+import gzip
+
 
 last_percent_reported = None
 
@@ -20,11 +33,21 @@ last_percent_reported = None
 def main ():
     with(open('config.json', 'r')) as f:
         config = json.load(f)
-    train_filename = maybe_download(config["train"]["images"], 9912422)
-    test_filename  = maybe_download(config["test"]["images"], 1648877)
 
-    train_folders = maybe_extract(train_filename)
-    test_folders = maybe_extract(test_filename)
+    train_filename_gz = maybe_download(config["train"]["images"], 9912422)
+    test_filename_gz  = maybe_download(config["test"]["images"], 1648877)
+
+    train_pickle = maybe_extract(train_filename_gz)
+    test_pickle = maybe_extract(test_filename_gz)
+
+    with open(train_pickle, 'rb') as tp:
+        train_dataset = pickle.load(tp)
+
+    with open(test_pickle, 'rb') as tp:
+        test_dataset = pickle.load(tp)
+
+    plt.imshow(train_dataset[0])
+    plt.show()
 
 
 def download_progress_hook(count, blockSize, totalSize):
@@ -42,7 +65,7 @@ def download_progress_hook(count, blockSize, totalSize):
 
     last_percent_reported = percent
 
-def maybe_download (filename, expected_bytes, force=True):
+def maybe_download (filename, expected_bytes, force=False):
     dest_filename = os.path.join(CACHE_DIR, filename)
     if force or not os.path.exists(dest_filename):
         print('Attempting to download:', filename)
@@ -55,25 +78,48 @@ def maybe_download (filename, expected_bytes, force=True):
         raise Exception('Failed to verify ' + dest_filename)
     return dest_filename
 
-def maybe_extract(filename, force=False):
-  root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
-  if os.path.isdir(root) and not force:
-    # You may override by setting force=True.
-    print('%s already present - Skipping extraction of %s.' % (root, filename))
-  else:
-    print('Extracting data for %s. This may take a while. Please wait.' % root)
-    tar = tarfile.open(filename)
-    sys.stdout.flush()
-    tar.extractall(data_root)
-    tar.close()
-  data_folders = [
-    os.path.join(root, d) for d in sorted(os.listdir(root))
-    if os.path.isdir(os.path.join(root, d))]
-  if len(data_folders) != num_classes:
-    raise Exception(
-      'Expected %d folders, one per class. Found %d instead.' % (
-        num_classes, len(data_folders)))
-  print(data_folders)
-  return data_folders
+def extract(filename, force=False):
+    root = os.path.splitext(os.path.splitext(filename)[0])[0]
+    fopen = gzip.open if os.path.splitext(filename)[1] == '.gz' else open
+    with fopen(filename, 'rb') as fd:
+        pickle_file = root + '.pickle'
+        return maybe_pickle(pickle_file, parse_idx(fd), force)
+
+
+def maybe_pickle(filename, data, force=False):
+    if (not os.path.exists(filename) or force):
+        with open(filename, 'wb') as pf:
+            print 'pickling', filename
+            pickle.dump(data, pf, pickle.HIGHEST_PROTOCOL)
+    return filename
+
+def parse_idx(fd):
+    header = fd.read(4)
+    if len(header) != 4:
+        raise IdxDecodeError('Invalid IDX file, file empty or does not contain a full header.')
+
+    zeros, data_type, num_dimensions = struct.unpack('>HBB', header)
+
+    if zeros != 0:
+        raise IdxDecodeError('Invalid IDX file, file must start with two zero bytes. '
+                             'Found 0x%02x' % zeros)
+
+    try:
+        data_type = DATA_TYPES[data_type]
+    except KeyError:
+        raise IdxDecodeError('Unknown data type 0x%02x in IDX file' % data_type)
+
+    dimension_sizes = struct.unpack('>' + 'I' * num_dimensions,
+                                    fd.read(4 * num_dimensions))
+
+    data = array.array(data_type, fd.read())
+    data.byteswap()  # looks like array.array reads data as little endian
+
+    expected_items = functools.reduce(operator.mul, dimension_sizes)
+    if len(data) != expected_items:
+        raise IdxDecodeError('IDX file has wrong number of items. '
+                             'Expected: %d. Found: %d' % (expected_items, len(data)))
+    return np.array(data).reshape(dimension_sizes)
+
 if __name__ == '__main__':
     main()

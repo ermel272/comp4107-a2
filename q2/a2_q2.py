72176c2834ffef5bc7ea305ee38ab276233ab184
Chris Ermel <ermel272@gmail.com>
2017-10-23 23:47:03 -0400

    redo kmeans

--- q2/a2_q2.py
+++ q2/a2_q2.py
@@ -37,37 +37,24 @@ def main():
         config = json.load(f)
 
     train_filename_gz = maybe_download(config['train']['images'], 9912422)
-    test_filename_gz = maybe_download(config['test']['images'], 1648877)
     train_labels_gz = maybe_download(config['train']['labels'], 28881)
-    test_labels_gz = maybe_download(config['test']['labels'], 4542)
-
     train_pickle = extract(train_filename_gz)
     train_labels_pickle = extract(train_labels_gz)
-    test_pickle = extract(test_filename_gz)
-    test_labels_pickle = extract(test_labels_gz)
-
     train_data = load_pickle(train_pickle)
     train_labels = load_pickle(train_labels_pickle)
-    test_data = load_pickle(test_pickle)
-    test_labels = load_pickle(test_labels_pickle)
 
     net = load_pickle(config['brain']['filename'])
 
     if net:
         print 'Using already existing neural network from %s' % config['brain']['filename']
+    else:
+        input_training = train_data.reshape(60000, 784)
 
-    # There are now 60,000 items of length 784 (28x28)
-    # This will serve as input to neural network
-    # Each cell will have 784 inputs
-    input_training = train_data.reshape(60000, 784)
-
-    if not net:
         # our system will be simple, one hidden layer
         net = RBFNetwork(learning_rate=.125)
-        net.add_layer(784)
 
+        # First layer containing 20 neurons derived from K-Means
         net.add_layer(20, gaussian)
-
         net.add_layer(10, gaussian)  # output layer
 
         net.train(input_training, train_labels)
--- q2/a2_q2_kmeans.py
+++ q2/a2_q2_kmeans.py
@@ -21,24 +21,32 @@ def main():
     train_pickle = extract(train_filename_gz)
     train_data = load_pickle(train_pickle)
 
-    # Only take the first 2000 data as a sample
-    input_training = train_data.reshape(60000, 784)[:2000]
+    input_training = train_data.reshape(60000, 784)[:500]
     distance_list = []
     whitened = whiten(input_training)
-    max_k = 30
-    for i in range(1, max_k + 1):
+    k_values = range(1, 401)
+    for i in k_values:
         print "Performing K-Means clustering with K = {}".format(i)
         centroids, distortion = kmeans(whitened, i)
-        total_dist = 0
+        clusters = [list() for centroid in centroids]
 
-        # Compute the distance from all data points to their respective centroids
+        # Sort vector's into their clusters
         for vector in input_training:
-            dist = __find_closest_centroid_distance(vector, centroids)
-            total_dist += dist
+            c = __find_closest_centroid(vector, centroids)
+            clusters[c].append(vector)
 
-        mean_dist = total_dist / len(input_training)
-        print "Mean distance of {} observed".format(mean_dist)
-        distance_list.append(mean_dist)
+        total_dist = 0
+        # Compute distance from each cluster's points to their centroid
+        for j in range(0, len(clusters)):
+            centroid_dist = 0
+
+            for vector in clusters[j]:
+                centroid_dist += norm(vector - centroids[j]) ** 2
+
+            total_dist += centroid_dist
+
+        print "Objective centroid distance of {} observed".format(total_dist)
+        distance_list.append(total_dist)
 
     plot = {"Distances": distance_list}
     fig, ax = plt.subplots()
@@ -47,14 +55,18 @@ def main():
     plt.show()
 
 
-def __find_closest_centroid_distance(vector, centroids):
-    smallest_distance = norm(vector - centroids[0])
+def __find_closest_centroid(vector, centroids):
+    centroid = 0
+    smallest_distance = norm(vector - centroids[0]) ** 2
 
     for i in range(1, len(centroids)):
-        dist = norm(vector - centroids[i])
-        smallest_distance = dist if dist < smallest_distance else smallest_distance
+        dist = norm(vector - centroids[i]) ** 2
 
-    return smallest_distance
+        if dist < smallest_distance:
+            smallest_distance = dist
+            centroid = i
+
+    return centroid
 
 
 if __name__ == '__main__':
